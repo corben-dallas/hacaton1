1377aa234942fc567fc371944ad0d4d53051ebc4
Corbens <albert.u1994@gmail.com>
2021-07-04 16:39:53 +0300

    task 2 fixed

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const array = [1,7,5,8,3];
 
 // function sortItems(array) {
 // 	for (let i = 0; i < array.length; i++) {
-// 		for (let j = 0; j < array.length - 1; j++) {
+// 		for (let j = 0; j < array.length; j++) {
 // 			if (array[j] > array[j + 1]) {
 // 				let temp = array[j];
 // 				array[j] = array[j + 1];
 // 				array[j + 1] = temp;
 // 			}
 // 		}
+
 // 	}
 
 // 	return array;
@@ -19,21 +20,33 @@ const array = [1,7,5,8,3];
 
 const sort2 = arr => {
 	let max = arr[0];
+	let index = 0;
 	let length = arr.length;
+	console.log(arr);
 
-	for(let i = 0; i < arr.length; i++) {
+	for(let i = 0; i < length; i++) {
 		for (let j = 1; j < length; j++) {
-			console.log(arr);
 			if (arr[j] > max) {
 				max = arr[j];
-				let temp = arr[j];
-				arr[j] = arr[arr.length - 1];
-				arr[arr.length - 1] = temp;
+				index = j;
 			}
 		}
+		let temp = arr[index];
+		arr[index] = arr[arr.length - index];
+		arr[arr.length - index] = temp;
+		max = arr[0];
 		length = length - 1;
 	}
 	return arr;
 }
+console.log('result', sort2(array));
 
-console.log('result', sort2(array))
+// /////////////////////////////////////////////////////
+
+
+
+
+
+// const treeA = {
+
+// };
