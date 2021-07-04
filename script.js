fc51f6dbf31e2e886e0ceeb995dd9e9ce85e93c5
Corbens <albert.u1994@gmail.com>
2021-07-04 17:34:01 +0300

    task 3 done

--- script.js
+++ script.js
@@ -18,34 +18,51 @@ const array = [1,7,5,8,3];
 
 // /////////////////////////////////////////////////////////////
 
-const sort2 = arr => {
-	let max = arr[0];
-	let index = 0;
-	let length = arr.length;
-	console.log(arr);
+// const sort2 = arr => {
+// 	let max = arr[0];
+// 	let index = 0;
+// 	let length = arr.length;
+// 	console.log(arr);
 
-	for(let i = 0; i < length; i++) {
-		for (let j = 1; j < length; j++) {
-			if (arr[j] > max) {
-				max = arr[j];
-				index = j;
-			}
-		}
-		let temp = arr[index];
-		arr[index] = arr[arr.length - index];
-		arr[arr.length - index] = temp;
-		max = arr[0];
-		length = length - 1;
-	}
-	return arr;
-}
-console.log('result', sort2(array));
+// 	for(let i = 0; i < length; i++) {
+// 		for (let j = 1; j < length; j++) {
+// 			if (arr[j] > max) {
+// 				max = arr[j];
+// 				index = j;
+// 			}
+// 		}
+// 		let temp = arr[index];
+// 		arr[index] = arr[arr.length - index];
+// 		arr[arr.length - index] = temp;
+// 		max = arr[0];
+// 		length = length - 1;
+// 	}
+// 	return arr;
+// }
+// console.log('result', sort2(array));
 
 // /////////////////////////////////////////////////////
 
+const sort3 = arr => {
+	for (var i = 0; i < arr.length; i++) { 
+		const k = arr[i];
+		let j = i;
+
+		while (j > 0 && arr[j - 1] > k) { 
+			arr[j] = arr[j - 1]; 
+			j--; 
+		}
+
+		arr[j] = k;
+	}
+
+	return arr;
+}
 
 
 
+const result = sort3(array);
+console.log('result', result);
 
 // const treeA = {
 
