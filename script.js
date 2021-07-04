e8474fa7e25cc7d0863f48b634e1bccd19656249
Corbens <albert.u1994@gmail.com>
2021-07-04 16:07:20 +0300

    task 2 done

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-const array = [5,3,-1,8,7,0,2];
+const array = [1,3,7,5,2,8,3];
 
-function sortItems(array) {
-	for (let i = 0; i < array.length; i++) {
-		for (let j = 0; j < array.length - 1; j++) {
-			if (array[j] > array[j + 1]) {
-				let temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-			}
-		}
-	}
+// function sortItems(array) {
+// 	for (let i = 0; i < array.length; i++) {
+// 		for (let j = 0; j < array.length - 1; j++) {
+// 			if (array[j] > array[j + 1]) {
+// 				let temp = array[j];
+// 				array[j] = array[j + 1];
+// 				array[j + 1] = temp;
+// 			}
+// 		}
+// 	}
 
-	return array;
-}
-console.log('result', sortItems(array));
+// 	return array;
+// }
+// console.log('result', sortItems(array));
 
 // /////////////////////////////////////////////////////////////
 
-// const sort2 = arr => {
-// 	const max = arr[0];
+const sort2 = arr => {
+	let max = arr[0];
+	let length = arr.length;
 
-// 	for(let i = 0; i < arr.length; )
-// }
+	for(let i = 0; i < arr.length; i++) {
+		for (let j = 0; j < length; j++) {
+			if (arr[j] > max) {
+				max = arr[j];
+				let temp = arr[j];
+				arr[j] = arr[arr.length - 1];
+				arr[arr.length - 1] = temp;
+			}
+		}
+		length = length - 1;
+	}
+	return arr;
+}
+
+console.log('result', sort2(array))
